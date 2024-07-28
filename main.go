31fff2b57550ea456f2ba3f3e737be59b9f8e93a
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-07-29 08:05:14 +1000

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 // Файл создан для укрепления умения беглого программирования и будет включать основные инструменты Go
 // Этот файл будет постепенно пополняться до 1 часа непрерывного коддинга при умении скоростной печати
-// Чтобы получить пользу от файла, вы должны начать кодить его содержимое фрагментами и усваивать что и как тут происходит
 // Первый блок кода читает файлы и выводит топ-10 слов
-// Запускаем с аргументом, примерно так: go run main.go /.short_files
+// Запускаем с аргументом, примерно так: go run main.go ./short_files
 // Символ ./ используется в bash-языке как символ относительного пути к текущему каталогу
 // ЗЫ - нужно создать файлы с содержимым
 
-// Лучшее время - 8 мин 18 сек
+// Лучшее время - 9 мин
 package main
 
 import (
@@ -18,65 +17,63 @@ import (
 	"strings"
 )
 
-type frequencyWord struct {
-	word      string
-	frequency int
-}
-
 func main() {
-	pathDirFiles := "./files"
+	pathDir := "./files"
 	if len(os.Args) == 2 {
-		pathDirFiles = os.Args[1]
+		pathDir = os.Args[1]
 	}
-	filesList, err := os.ReadDir(pathDirFiles)
+	filesList, err := os.ReadDir(pathDir)
 	if err != nil {
-		log.Println("не удаётся найти каталог:", err)
+		log.Println(err)
 		return
 	}
-
-	allWordsSlice := make([]string, 0)
+	allWordsSlice := make([]string, 0, 10)
 	for _, fileEntry := range filesList {
 		if fileEntry.IsDir() {
 			continue
 		}
-		fullFilePath := filepath.Join(pathDirFiles, fileEntry.Name())
-		contentFile, err := os.ReadFile(fullFilePath)
+		fullPathFile := filepath.Join(pathDir, fileEntry.Name())
+		contentFile, err := os.ReadFile(fullPathFile)
 		if err != nil {
-			log.Printf("не удалось прочитать файл %s: %s", fileEntry.Name(), err)
+			log.Printf("содержимое файла %s не удалось прочитать: %s", fileEntry.Name(), err)
 			continue
 		}
 		words := strings.Fields(string(contentFile))
 		allWordsSlice = append(allWordsSlice, words...)
 	}
-
 	allWordsMap := make(map[string]int)
-	for _, value := range allWordsSlice {
-		allWordsMap[value]++
+	for _, val := range allWordsSlice {
+		allWordsMap[val]++
 	}
-
-	frequencyWordsSlice := make([]frequencyWord, 0)
-	for key, value := range allWordsMap {
-		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{key, value})
+	type frequencyWord struct {
+		word      string
+		frequency int
 	}
-
+	frequencyWordsSlice := make([]frequencyWord, 0, 10)
+	for key, val := range allWordsMap {
+		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{key, val})
+	}
 	sort.Slice(frequencyWordsSlice, func(i, j int) bool {
 		return frequencyWordsSlice[i].frequency > frequencyWordsSlice[j].frequency
 	})
-	var top, currentFrequency, lastFrecuency int
+	var currentFrequency, lastFrequency, topWord int
 	var buf []string
 	for _, elem := range frequencyWordsSlice {
-		if top > 10 {
+		if topWord > 10 {
 			break
 		}
 		currentFrequency = elem.frequency
-		if len(buf) > 0 && currentFrequency != lastFrecuency {
-			fmt.Printf("Топ %d частоты слов встречается по %d р.:\t%v\n", top, lastFrecuency, buf)
+		if currentFrequency != lastFrequency && len(buf) > 0 {
+			fmt.Printf("\tТоп №%d состоит из %d слов, которые встречаются по %d р.:\t%s\n", topWord, len(buf), lastFrequency, buf)
 			buf = nil
 		}
-		if currentFrequency != lastFrecuency {
-			top++
-			lastFrecuency = currentFrequency
+		if currentFrequency != lastFrequency {
+			lastFrequency = currentFrequency
+			topWord++
 		}
 		buf = append(buf, elem.word)
+	}
+	if len(buf) > 0 && topWord < 11 {
+		fmt.Printf("\tТоп №%d состоит из %d слов, которые встречаются по %d р.\n", topWord, len(buf), lastFrequency)
 	}
 }
