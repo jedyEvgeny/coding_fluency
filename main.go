f0a9b91b8d9f652266aa1d24a9353a22689c5656
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-07-31 10:26:56 +1000

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@
 // Символ ./ используется в bash-языке как символ относительного пути к текущему каталогу
 // ЗЫ - нужно создать файлы с содержимым
 
-// Лучшее время - 11 мин 33 сек
+// Лучшее время - 11 мин 12 сек
 package main
 
 import (
@@ -21,9 +21,25 @@ import (
 )
 
 var (
-	mu sync.RWMutex
 	wg sync.WaitGroup
+	mu sync.Mutex
 )
+
+func findAllWords(pathDir string, slice *[]string, entry fs.DirEntry) {
+	defer wg.Done()
+	fullFilePath := filepath.Join(pathDir, entry.Name())
+	contentFile, err := os.ReadFile(fullFilePath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	words := strings.FieldsFunc(string(contentFile), func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+	mu.Lock()
+	*slice = append(*slice, words...)
+	mu.Unlock()
+}
 
 func main() {
 	pathDir := "./files"
@@ -40,57 +56,45 @@ func main() {
 			continue
 		}
 		wg.Add(1)
-		go func(entry fs.DirEntry) {
-			defer wg.Done()
-			fullFilePath := filepath.Join(pathDir, entry.Name())
-			contentFile, err := os.ReadFile(fullFilePath)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			words := strings.FieldsFunc(string(contentFile), func(r rune) bool {
-				return !unicode.IsLetter(r) && !unicode.IsNumber(r)
-			})
-			mu.Lock()
-			allWordsSlice = append(allWordsSlice, words...)
-			mu.Unlock()
-		}(fileEntry)
+		go findAllWords(pathDir, &allWordsSlice, fileEntry)
 	}
 	wg.Wait()
 	allWordsMap := make(map[string]int)
 	for _, el := range allWordsSlice {
 		allWordsMap[el]++
 	}
-	type frequncyWord struct {
+	type frequencyWord struct {
 		word      string
 		frequency int
 	}
-	frequencyWordsSlice := make([]frequncyWord, 0, 10)
+	frequencyWordsSlice := make([]frequencyWord, 0, 10)
 	for key, val := range allWordsMap {
-		frequencyWordsSlice = append(frequencyWordsSlice, frequncyWord{key, val})
+		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{key, val})
 	}
 	sort.Slice(frequencyWordsSlice, func(i, j int) bool {
 		return frequencyWordsSlice[i].frequency > frequencyWordsSlice[j].frequency
 	})
-	var currentFrequency, lastFrequency, topWords int
+
+	var currentFrequency, lastFrequency, topWord int
 	buf := make([]string, 0, 10)
 	for _, el := range frequencyWordsSlice {
-		if topWords > 10 {
+		if topWord > 10 {
 			break
 		}
 		currentFrequency = el.frequency
 		if currentFrequency != lastFrequency && len(buf) > 0 {
-			fmt.Printf("Топ №%d состоит из %d слов, которые встречаются %d р.: %s\n", topWords, len(buf), lastFrequency, buf)
+			fmt.Printf("Топ №%d состоит из %d слов, которые встречаются по %d р.: %s\n", topWord, len(buf), lastFrequency, buf)
 			buf = nil
 		}
 		if currentFrequency != lastFrequency {
 			lastFrequency = currentFrequency
-			topWords++
+			topWord++
 		}
 		buf = append(buf, el.word)
 	}
-	if len(buf) > 0 && topWords < 11 {
-		fmt.Printf("Топ №%d состоит из %d слов, которые встречаются %d р.\n", topWords, len(buf), lastFrequency)
+	if len(buf) > 0 && topWord < 11 {
+		fmt.Printf("Топ №%d состоит из %d слов, которые встречаются по %d р.\n", topWord, len(buf), lastFrequency)
 	}
 }
+
 
