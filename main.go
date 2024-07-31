761d897af02e5f98f5585416dc5530a9c16fe14c
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-07-31 19:31:57 +1000

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,10 @@
 // Первый блок кода читает файлы и выводит топ-10 слов
 // Запускаем с аргументом, примерно так: go run main.go ./short_files
 // Символ ./ используется в bash-языке как символ относительного пути к текущему каталогу
-// ЗЫ - нужно создать файлы с содержимым
+// ЗЫ - нужно заранее создать файлы с содержимым, а также go mod для тестов
+// ЗЗЫ - начинаю отсчёт времени с создания файлов .go, котороые создаю через консоль touch main.go 
 
-// Лучшее время - 11 мин 12 сек пока без теста
+// Лучшее время с тестами - 20 мин 25 сек
 package main
 
 import (
@@ -25,15 +26,15 @@ var (
 	mu sync.Mutex
 )
 
-func findAllWords(pathDir string, slice *[]string, entry fs.DirEntry) {
+func findAllWords(slice *[]string, dir string, entry fs.DirEntry) {
 	defer wg.Done()
-	fullFilePath := filepath.Join(pathDir, entry.Name())
-	contentFile, err := os.ReadFile(fullFilePath)
+	fullFilePath := filepath.Join(dir, entry.Name())
+	content, err := os.ReadFile(fullFilePath)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	words := strings.FieldsFunc(string(contentFile), func(r rune) bool {
+	words := strings.FieldsFunc(string(content), func(r rune) bool {
 		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
 	})
 	mu.Lock()
@@ -56,7 +57,7 @@ func main() {
 			continue
 		}
 		wg.Add(1)
-		go findAllWords(pathDir, &allWordsSlice, fileEntry)
+		go findAllWords(&allWordsSlice, pathDir, fileEntry)
 	}
 	wg.Wait()
 	allWordsMap := make(map[string]int)
@@ -74,27 +75,24 @@ func main() {
 	sort.Slice(frequencyWordsSlice, func(i, j int) bool {
 		return frequencyWordsSlice[i].frequency > frequencyWordsSlice[j].frequency
 	})
-
-	var currentFrequency, lastFrequency, topWord int
+	var currentFrequency, lastFrequency, topWords int
 	buf := make([]string, 0, 10)
 	for _, el := range frequencyWordsSlice {
-		if topWord > 10 {
+		if topWords > 10 {
 			break
 		}
 		currentFrequency = el.frequency
 		if currentFrequency != lastFrequency && len(buf) > 0 {
-			fmt.Printf("Топ №%d состоит из %d слов, которые встречаются по %d р.: %s\n", topWord, len(buf), lastFrequency, buf)
+			fmt.Printf("Топ №%d состоит из %d слов, встречающихся по %d р.: %s\n", topWords, len(buf), lastFrequency, buf)
 			buf = nil
 		}
 		if currentFrequency != lastFrequency {
 			lastFrequency = currentFrequency
-			topWord++
+			topWords++
 		}
 		buf = append(buf, el.word)
 	}
-	if len(buf) > 0 && topWord < 11 {
-		fmt.Printf("Топ №%d состоит из %d слов, которые встречаются по %d р.\n", topWord, len(buf), lastFrequency)
+	if len(buf) > 0 && topWords < 11 {
+		fmt.Printf("Топ №%d состоит из %d слов, встречающихся по %d р.\n", topWords, len(buf), lastFrequency)
 	}
 }
-
-
