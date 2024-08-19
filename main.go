e2cb65e2a0716cab2f187905f65a2d048752d7f8
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-08-19 12:07:38 +1000

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,9 @@
 // ЗЫ - нужно заранее создать файлы с содержимым, а также go mod для тестов
 // ЗЗЫ - начинаю отсчёт времени с создания файлов .go, котороые создаю через консоль touch main.go 
 
-// Лучшее время: - 26 мин 45 сек, включая удаление предыдущих файлов main и main_test через терминал, запуск приложения в терминале, проверка ответа в браузере
-// Последнее время: 29 мин 27 сек.
+// Лучшее время: - 41 мин, включая удаление предыдущих файлов main и main_test через терминал, 
+// запуск приложения в терминале, проверка ответа в браузере, запуск тестов. Импорты подтягиваю автоматически
+
 package main
 
 import (
@@ -26,77 +27,121 @@ import (
 )
 
 var (
-	wg sync.WaitGroup
-	mu sync.Mutex
+	filesDir = "./files"
+	endpoint = "/words"
+	port     = ":8080"
+	topWords = 10
+	host     = "api.telegram.org"
+	method   = "getUpdates"
+	scheme   = "https"
 )
 
+type App struct {
+	FilesDir    string
+	Endpoint    string
+	Port        string
+	Host        string
+	Method      string
+	Scheme      string
+	mu          sync.Mutex
+	wg          sync.WaitGroup
+	maxTopWords int
+}
+
 func main() {
-	filesDir := "./files"
+	a := New()
+	err := a.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func New() App {
+	return App{
+		FilesDir:    dir(),
+		Port:        port,
+		Endpoint:    endpoint,
+		maxTopWords: topWords,
+		Host:        host,
+		Method:      method,
+		Scheme:      scheme,
+	}
+}
+
+func dir() string {
 	if len(os.Args) == 2 {
 		filesDir = os.Args[1]
 	}
-	filesList, err := os.ReadDir(filesDir)
+	return filesDir
+}
+
+func (a *App) Run() error {
+	filesList, err := os.ReadDir(a.FilesDir)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	allWordsSlice := make([]string, 0, 10)
 	for _, fileEntry := range filesList {
 		if fileEntry.IsDir() {
 			continue
 		}
-		wg.Add(1)
-		go findAllWords(&allWordsSlice, filesDir, fileEntry)
+		a.wg.Add(1)
+		go a.findAllWords(&allWordsSlice, fileEntry)
 	}
-	wg.Wait()
+	a.wg.Wait()
+
 	allWordsMap := make(map[string]int)
 	for _, el := range allWordsSlice {
 		allWordsMap[el]++
 	}
 	type frequencyWord struct {
-		word      string
 		frequency int
+		word      string
 	}
 	frequencyWordsSlice := make([]frequencyWord, 0, 10)
 	for key, val := range allWordsMap {
-		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{key, val})
+		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{val, key})
 	}
 	sort.Slice(frequencyWordsSlice, func(i, j int) bool {
 		return frequencyWordsSlice[i].frequency > frequencyWordsSlice[j].frequency
 	})
+
 	var currentFrequency, lastFrequency, topWords int
 	buf := make([]string, 0, 10)
-	var result string
+	result := ""
+
 	for _, el := range frequencyWordsSlice {
-		if topWords > 10 {
+		if topWords > a.maxTopWords {
 			break
 		}
 		currentFrequency = el.frequency
 		if currentFrequency != lastFrequency && len(buf) > 0 {
 			str := fmt.Sprintf("Топ №%d состоит из %d слов, встречающихся по %d р.: %s\n", topWords, len(buf), lastFrequency, buf)
-			fmt.Print(str)
 			result += str
+			fmt.Print(str)
 			buf = nil
 		}
 		if currentFrequency != lastFrequency {
-			lastFrequency = currentFrequency
 			topWords++
+			lastFrequency = currentFrequency
 		}
 		buf = append(buf, el.word)
 	}
-	if len(buf) > 0 && topWords < 11 {
+	if len(buf) > 0 && topWords < a.maxTopWords+1 {
 		str := fmt.Sprintf("Топ №%d состоит из %d слов, встречающихся по %d р.\n", topWords, len(buf), lastFrequency)
-		fmt.Print(str)
 		result += str
+		fmt.Print(str)
 	}
 
-	createRequest()
-	createServer(result)
+	a.createRequest()
+	a.createServer(result)
+	return nil
 }
 
-func findAllWords(slice *[]string, path string, entry fs.DirEntry) {
-	defer wg.Done()
-	fullFName := filepath.Join(path, entry.Name())
-	content, err := os.ReadFile(fullFName)
+func (a *App) findAllWords(slice *[]string, entry fs.DirEntry) {
+	defer a.wg.Done()
+	fullFPath := filepath.Join(a.FilesDir, entry.Name())
+	content, err := os.ReadFile(fullFPath)
 	if err != nil {
 		log.Println(err)
 		return
@@ -104,217 +149,43 @@ func findAllWords(slice *[]string, path string, entry fs.DirEntry) {
 	words := strings.FieldsFunc(string(content), func(r rune) bool {
 		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
 	})
-	mu.Lock()
+	a.mu.Lock()
 	*slice = append(*slice, words...)
-	mu.Unlock()
+	a.mu.Unlock()
 }
 
-func createRequest() {
-	host := "api.telegram.org"
-	basePath := "bot" + "1234567890"
-	method := "getUpdates"
+func (a *App) createRequest() {
 	u := url.URL{
-		Scheme: "https",
-		Host:   host,
-		Path:   path.Join(basePath, method),
+		Scheme: a.Scheme,
+		Path:   a.basePath(),
+		Host:   a.Host,
 	}
 	log.Println(u.String())
 	req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
 	log.Println(req)
 
 	query := url.Values{}
-	query.Add("chat_id", "1234560")
-	query.Add("text", "Hello, Telegram!")
+	query.Add("chat_id", "12345670")
+	query.Add("text", "Hello, Telegram")
 	log.Println(query)
+
 	req.URL.RawQuery = query.Encode()
 	log.Println(req)
 }
 
-func createServer(result string) {
-	handler := func(w http.ResponseWriter, r *http.Request) { handWords(w, r, result) }
-	http.HandleFunc("/words", handler)
-	log.Println("Запускаем сервер:")
-	http.ListenAndServe(":8080", nil)
+func (a *App) basePath() string {
+	basePath := "bot" + "123456"
+	return path.Join(a.Method, basePath)
 }
 
-//Рефакторинг кода: 
-package main
+func (a *App) createServer(result string) {
+	handler := func(w http.ResponseWriter, r *http.Request) { handleWords(w, r, result) }
+	log.Println("Начинаем слушать сервер:")
+	http.HandleFunc(a.Endpoint, handler)
+	http.ListenAndServe(a.Port, nil)
+}
 
-// import (
-// 	"fmt"
-// 	"io/fs"
-// 	"log"
-// 	"net/http"
-// 	"net/url"
-// 	"os"
-// 	"path"
-// 	"path/filepath"
-// 	"sort"
-// 	"strings"
-// 	"sync"
-// 	"unicode"
-// )
-
-// var (
-// 	filesDirectory = "./files"
-// 	endpoint       = "/words"
-// 	port           = "0.0.0.0:8080"
-// )
-
-// type App struct {
-// 	mu       sync.Mutex
-// 	wg       sync.WaitGroup
-// 	filesDir string
-// 	endpoint string
-// 	port     string
-// }
-
-// func main() {
-// 	a := New()
-// 	err := a.Run()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// func New() App {
-// 	return App{
-// 		filesDir: newBasePath(),
-// 		endpoint: endpoint,
-// 		port:     port,
-// 	}
-// }
-
-// func newBasePath() string {
-// 	if len(os.Args) == 2 {
-// 		filesDirectory = os.Args[1]
-// 	}
-// 	return filesDirectory
-// }
-
-// func (a *App) Run() error {
-// 	result, err := a.Words()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	createRequest()
-// 	a.createServer(result)
-// 	return nil
-// }
-
-// func (a *App) Words() (string, error) {
-// 	filesList, err := os.ReadDir(a.filesDir)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	allWordsSlice := make([]string, 0, 10)
-// 	for _, fileEntry := range filesList {
-// 		if fileEntry.IsDir() {
-// 			continue
-// 		}
-// 		a.wg.Add(1)
-// 		go a.findAllWords(&allWordsSlice, a.filesDir, fileEntry)
-// 	}
-// 	a.wg.Wait()
-
-// 	allWordsMap := make(map[string]int)
-// 	for _, el := range allWordsSlice {
-// 		allWordsMap[el]++
-// 	}
-
-// 	type frequencyWord struct {
-// 		word      string
-// 		frequency int
-// 	}
-// 	frequencyWordsSlice := make([]frequencyWord, 0, 10)
-// 	for key, val := range allWordsMap {
-// 		frequencyWordsSlice = append(frequencyWordsSlice, frequencyWord{key, val})
-// 	}
-// 	sort.Slice(frequencyWordsSlice, func(i, j int) bool {
-// 		return frequencyWordsSlice[i].frequency > frequencyWordsSlice[j].frequency
-// 	})
-// 	var currentFrequency, lastFrequency, topWords int
-// 	buf := make([]string, 0, 10)
-// 	var result string
-// 	maxCount := 10
-// 	for _, el := range frequencyWordsSlice {
-// 		if topWords > maxCount+1 {
-// 			break
-// 		}
-// 		currentFrequency = el.frequency
-// 		if currentFrequency != lastFrequency && len(buf) > 0 {
-// 			str := fmt.Sprintf("Топ №%d состоит из %d слов, встречающихся по %d р.: %s\n", topWords, len(buf), lastFrequency, buf)
-// 			result += str
-// 			fmt.Print(str)
-// 			buf = nil
-// 		}
-// 		if currentFrequency != lastFrequency {
-// 			topWords++
-// 			lastFrequency = currentFrequency
-// 		}
-// 		buf = append(buf, el.word)
-// 	}
-// 	if len(buf) > 0 && topWords < maxCount {
-// 		str := fmt.Sprintf("Топ №%d состоит из %d слов, встречающихся по %d р.\n", topWords, len(buf), lastFrequency)
-// 		result += str
-// 		fmt.Print(str)
-// 	}
-// 	return result, nil
-// }
-
-// func (a *App) findAllWords(slice *[]string, path string, entry fs.DirEntry) {
-// 	defer a.wg.Done()
-// 	fullFPath := filepath.Join(path, entry.Name())
-// 	content, err := os.ReadFile(fullFPath)
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-// 	words := strings.FieldsFunc(string(content), func(r rune) bool {
-// 		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
-// 	})
-// 	a.mu.Lock()
-// 	*slice = append(*slice, words...)
-// 	a.mu.Unlock()
-// }
-
-// func createRequest() {
-// 	host := "api.telegram.org"
-// 	basePath := "bot" + "1234567890"
-// 	method := "getUpdates"
-
-// 	u := url.URL{
-// 		Scheme: "https",
-// 		Host:   host,
-// 		Path:   path.Join(basePath, method),
-// 	}
-// 	log.Println(u.String())
-// 	log.Println(u)
-// 	req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
-// 	log.Println(req)
-
-// 	query := url.Values{}
-// 	query.Add("chat_id", "12345670")
-// 	query.Add("text", "Hello, Telegram!")
-// 	log.Println(query)
-
-// 	req.URL.RawQuery = query.Encode()
-// 	log.Println(req)
-// }
-
-// func (a *App) createServer(result string) {
-// 	handler := func(w http.ResponseWriter, r *http.Request) { handleWords(w, r, result) }
-// 	http.HandleFunc(a.endpoint, handler)
-// 	log.Println("Слушаем порт на сервере:")
-// 	http.ListenAndServe(a.port, nil)
-// }
-
-// func handleWords(w http.ResponseWriter, _ *http.Request, result string) {
-// 	w.Header().Set("Content-type", "text/plain; charset=utf-8")
-// 	fmt.Fprint(w, result)
-// }
-
-func handWords(w http.ResponseWriter, _ *http.Request, result string) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func handleWords(w http.ResponseWriter, _ *http.Request, result string) {
+	w.Header().Set("Content-type", "text/plaing; charset=utf-8")
 	fmt.Fprint(w, result)
 }
