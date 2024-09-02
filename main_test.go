274c4f76b07cb4e06b772cc433ae9b8b78ef1878
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-09-02 13:54:47 +1000

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -12,15 +12,15 @@ func TestFindWords(t *testing.T) {
 	tempDir := t.TempDir()
 	content := []string{
 		"БИМ бом",
-		"Дин доН",
-		"кин, кон?",
+		"диН Дон",
+		"кин, !кон",
 		"apple+banana",
 		"",
 		" ",
 		"	",
-		"1 -22 ;333",
+		"1 -22 :333",
 	}
-	expectedWords := []string{"БИМ", "бом", "Дин", "доН", "кин", "кон", "apple", "banana", "1", "22", "333"}
+	expectedSlice := []string{"БИМ", "бом", "диН", "Дон", "кин", "кон", "apple", "banana", "1", "22", "333"}
 	for idx, el := range content {
 		fName := fmt.Sprintf("%d.txt", idx)
 		fPath := filepath.Join(tempDir, fName)
@@ -29,19 +29,20 @@ func TestFindWords(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
+	var allWords []string
 	filesList, err := os.ReadDir(tempDir)
 	if err != nil {
 		t.Fatal(err)
 	}
-	var allWords []string
 	a := App{
 		filesDir: tempDir,
 	}
 	for _, entry := range filesList {
+		a.wg.Add(1)
 		allWords = a.findWords(allWords, entry)
 	}
-	if !reflect.DeepEqual(expectedWords, allWords) {
-		t.Errorf("Ожидалось \n%s,\nполучили \n%s\n", expectedWords, allWords)
+	a.wg.Wait()
+	if !reflect.DeepEqual(expectedSlice, allWords) {
+		t.Errorf("Ожидалось \n%s,\nПолучили \n%s\n", expectedSlice, allWords)
 	}
-
 }
