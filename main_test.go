dcec01c084b31707f973d0287cdc15b7f0073b70
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-09-04 12:01:24 +1000

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -12,45 +12,45 @@ func TestFindWords(t *testing.T) {
 	tempDir := t.TempDir()
 	content := []string{
 		"БИМ бом",
-		"Дин доН",
-		"кин, ?кон",
-		"apple*banana",
+		"диН Дон",
+		",кин ?кон",
+		"apple+banana",
 		"",
 		" ",
 		"	",
-		"1 -22 333_",
+		"1 -22 _333%",
 	}
-	expectedWords := []string{"БИМ", "бом", "Дин", "доН", "кин", "кон", "apple", "banana", "1", "22", "333"}
+	expectedWords := []string{"БИМ", "бом", "диН", "Дон", "кин", "кон", "apple", "banana", "1", "22", "333"}
 	for idx, el := range content {
 		fName := fmt.Sprintf("%d.txt", idx)
 		fPath := filepath.Join(tempDir, fName)
 		err := os.WriteFile(fPath, []byte(el), perm)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("не удалось создать тестовый файл №%d: %v", idx, err)
 		}
 	}
 	var allWords []string
 	a := App{filesDir: tempDir}
 	filesList, err := os.ReadDir(tempDir)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("не удалось прочитать директорию %s: %v", tempDir, err)
 	}
 	for _, entry := range filesList {
 		allWords = a.findWords(allWords, entry)
 	}
 	if !reflect.DeepEqual(expectedWords, allWords) {
-		t.Errorf("Ожидалось:\n%s,\nполучили:\n%s\n", expectedWords, allWords)
+		t.Errorf("Ожидалось:\n%s,\nПолучили:\n%s\n", expectedWords, allWords)
 	}
 }
 
-func BenchmarkFindWords(b *testing.B) {
-	dir := "./files"
-	a := App{filesDir: dir}
+func Benchmark(b *testing.B) {
+	filesDir := "./files"
+	a := App{filesDir: filesDir}
+	var allWords []string
 	filesList, err := os.ReadDir(a.filesDir)
 	if err != nil {
-		b.Fatal(err)
+		b.Fatalf("не удалось прочитать директорию %s: %v", a.filesDir, err)
 	}
-	var allWords []string
 
 	for i := 0; i < b.N; i++ {
 		allWords = allWords[:0]
